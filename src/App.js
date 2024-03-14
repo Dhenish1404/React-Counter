e85b18727616623302ae19311dbb68d5757ab5d6
dhenish <dhenish.jivani@vijigi.com>
2024-03-14 14:06:43 +0530

    Make counter with Increase and Decrease functionality with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 function App() {
+  const [value, setValue] = useState(3);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center mt-5">
+      <h1 className="mb-4">Let's start the React with basic Counter project</h1>
+      <div className="row justify-content-center mb-4">
+        <div className="col-md-6 mb-2">
+          <button type="button" className="btn btn-outline-success w-100 px-4 py-2" onClick={() => value < 10 ? setValue(value + 1) : alert("Value can't go more then 10")}>
+            Increase
+          </button>
+        </div>
+        <div className="col-md-6 mb-2">
+          <button type="button" className="btn btn-outline-danger w-100 px-4 py-2" onClick={() => value > 0 ? setValue(value - 1) : alert("Value must be positive, can't go negative.")}>
+            Decrease
+          </button>
+        </div>
+      </div>
+      <h3 className="text-info">Value: {value}</h3>
     </div>
   );
 }
